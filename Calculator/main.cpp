d1edb2aafe04c87efcb309eac8d7b96ba78fd1db
Maria <mamarter@users.noreply.github.com>
2015-12-09 23:17:11 +0000

    First version of calculator
    Implemented first functional version with symbols +-*/()

--- Calculator/main.cpp
+++ Calculator/main.cpp
@@ -7,14 +7,13 @@
 int main()
 {
 	Calculator calculator;
-	string str = "1+ 2*3- 4";
-	vector<string> str_tokenized;
-	int num = calculator.Tokenize(str, str_tokenized);
-	
-	while (true)
-	{
+	string input;
 
+	while (getline(cin, input))
+	{
+		calculator.EvaluateExpression(input);
 	}
+
 
 	return EXIT_SUCCESS;
 }
